472471fec2add63ab1c33067342e358142e71c48
cmf2201 <73976000+cmf2201@users.noreply.github.com>
2023-06-15 13:38:18 -1000

    Updated Main

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ def print_Hi(name):
 
 if __name__ == '__main__':
     print_Hi('PyCharm')
+    print_Hi('gamer')
 # Press the green button in the gutter to run the script.
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
